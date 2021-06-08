d0d4faa1eab6290360869763837e4324efe4e38c
Tianyu <ztyame@gmail.com>
2021-06-08 23:09:16 +0800

    Test ribbon for load balance

--- news-portal-dev-service-article/src/main/java/com/site/article/Application.java
+++ news-portal-dev-service-article/src/main/java/com/site/article/Application.java
@@ -1,8 +1,10 @@
 package com.site.article;
 
+import com.ribbon.rule.MyRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.ComponentScan;
 import tk.mybatis.spring.annotation.MapperScan;
 
@@ -10,6 +12,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 @MapperScan(basePackages = "com.site.article.mapper")
 @ComponentScan(basePackages = {"com.site", "org.n3r.idworker"})
 @EnableEurekaClient
+//@RibbonClient(name = "service-user", configuration = MyRule.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
